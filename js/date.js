a41589194fcf2f93cac540f1539f5d7c3fcb0ad3
Dream <563642668@qq.com>
2016-04-02 23:27:13 +0800

    。。

--- js/date.js
+++ js/date.js
@@ -1,18 +1,17 @@
-function Data () {
-			var dd=document.getElementById("data");
+function showTime () {
+			var dd=document.getElementById("show");
 			var myDate=new Date();
 			var year=myDate.getFullYear();
 			var month=myDate.getMonth()+1;
-			var data=myDate.getDate();
+			var date=myDate.getDate();
 			var week=new Array('星期日','星期一','星期二','星期三','星期四','星期五','星期六');
 			var h=myDate.getHours();
 			var min=myDate.getMinutes();
 			var sen=myDate.getSeconds();
-			
 			var m=chickTime(min);
 			var s=chickTime(sen);
-			dd.innerHTML=year+'年'+month+'月'+data+'日&nbsp'+week[myDate.getDate()]+h+':'+m+':'+s;
-			setTimeout(Data,500);
+			dd.innerHTML=year+'年'+month+'月'+date+'日&nbsp'+week[myDate.getDate()]+h+':'+m+':'+s;
+			setTimeout(showTime,500);
 		}
 function chickTime (i) {
 			if(i<10){
