8444bfbf2318e491e94e8842c7ff1bc8b2c5eddf
Dream <563642668@qq.com>
2016-04-03 01:08:53 +0800

    精确到秒

--- js/date.js
+++ js/date.js
@@ -19,4 +19,24 @@ function chickTime (i) {
 			}
 			return i;
 		}
-
+function FreshTime(){
+	var nowtime=new Date();
+		//	var end=new Date("2018,6,6"); //ie不兼容
+			var end=new Date();
+				end.setFullYear(2018);
+				end.setMonth(5);
+				end.setDate(6); 
+				end.setHours(8);
+				end.setMinutes(0);
+				end.setSeconds(0);
+			var lefttime=parseInt((end.getTime()-nowtime.getTime())/1000) ; 
+        	var d= parseInt(lefttime/(60*60*24));
+        	var h= parseInt((lefttime/(60*60))%24);
+        	var m= parseInt((lefttime/60)%60);
+        	var s= parseInt(lefttime%60) ;
+		//	var left=Math.ceil((end.getTime()-now.getTime())/(24*60*60*1000));
+			var sh=document.getElementById("show1");
+			sh.innerHTML=d+'天'+h+'小时'+m+'分'+s+'秒';
+			setTimeout(FreshTime,1000);
+		//	clearInterval(timer);
+}
